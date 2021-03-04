342d10625ef3a9003387b0db18b6ab19c8265478
ShuwenHe <1201220707@pku.edu.cn>
2021-03-04 10:24:16 +0800

    feat: initRedis

---
+++ initRedis/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
+var redisdb *redis.Client
+
+func initRedis() (err error) {
+	redisdb = redis.NewClient(&redis.Options{
+		Addr:     "62.234.11.179:6379",
+		Password: "",
+		DB:       0,
+	})
+	_, err = redisdb.Ping().Result()
+	return
+}
+
+func main() {
+	err := initRedis()
+	if err != nil {
+		fmt.Printf("connet redis failed,err:%v\n", err)
+		return
+	}
+	fmt.Println("redis connect success!")
+}
